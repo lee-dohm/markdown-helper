73cf12b41d844d4fe6f194aac21a1267910efafd
Lee Dohm <lee@lee-dohm.com>
2016-08-04 19:48:46 -0700

    Add test for error conditions

--- spec/link-collection.spec.js
+++ spec/link-collection.spec.js
@@ -35,4 +35,9 @@ describe('LinkCollection', function () {
   it('dasherizes the link text to get the ID', function () {
     expect(collection.convert('[foo bar baz](bar)')).to.equal('[foo bar baz][foo-bar-baz]')
   })
+
+  it('does not add invalid links to the list of references', function () {
+    expect(() => { collection.convert('') }).to.throw(Error)
+    expect(collection.getReferences()).to.eql([])
+  })
 })
